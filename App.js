c3b4b718d274eb27d480c2cb7402d302b15c4458
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-05-03 01:19:36 +0300

    add register screen styles

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, ImageBackground } from 'react-native';
+
+import { RegistrationScreen } from './src/screens/RegistrationScreen';
+import BackgroundPhoto from './src/images/RegistartionScreen/bg.jpg';
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<ImageBackground resizeMode="cover" source={BackgroundPhoto} style={styles.image}>
+				<RegistrationScreen />
+				{/* <StatusBar style="auto" /> */}
+			</ImageBackground>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+
+	},
+	image: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ src/screens/RegistrationScreen.jsx
@@ -0,0 +1,133 @@
+import styled from '@emotion/native';
+import { useFonts } from 'expo-font';
+import {
+    Alert,
+    Image,
+    Pressable,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+} from 'react-native';
+
+import AddIcon from '../images/RegistartionScreen/add.png';
+
+const Container = styled(View)`
+    position: relative;
+    width: 100%;
+    height: 549px;
+    padding-top: 92px;
+    background-color: #ffffff;
+    border-top-right-radius: 25px;
+    border-top-left-radius: 25px;
+`;
+const PhotoBox = styled(View)`
+    position: absolute;
+    top: -60px;
+    left: 50%;
+    transform: translateX(-60px);
+    height: 120px;
+    width: 120px;
+    background-color: #f6f6f6;
+    border-radius: 16px;
+`;
+
+const AddAvatarBtn = styled(Pressable)`
+    position: absolute;
+    right: -12px;
+    bottom: 14px;
+    width: 25px;
+    height: 25px;
+`;
+
+const FormWrapper = styled(View)`
+    padding: 0 16px;
+`;
+const TextCenter = styled(Text)`
+    margin-bottom: 33px;
+    font-size: 30px;
+    font-family: 'Roboto-bold';
+    text-align: center;
+    letter-spacing: ${0.01 * 30};
+    line-height: ${35 / 30};
+`;
+
+const Input = styled(TextInput)`
+    height: 50px;
+    background-color: #f6f6f6;
+    margin-bottom: 16px;
+    padding: 16px;
+    &:last-child {
+        /* margin-bottom: 43px; */
+    }
+`;
+
+const RegisterButton = styled(Pressable)`
+    margin-bottom: 16px;
+    padding: 16px;
+    background-color: #ff6c00;
+    border-radius: 100px;
+`;
+
+const RegisterButtonText = styled(Text)`
+    font-family: 'Roboto-regular';
+    text-align: center;
+    color: #ffffff;
+    font-size: 16px;
+    line-height: ${19 / 16};
+`;
+
+const LoginNavigateButtonText = styled(Text)`
+    font-family: 'Roboto-regular';
+    color: #1b4371;
+    text-align: center;
+    font-size: 16px;
+    line-height: ${19 / 16};
+`;
+
+export function RegistrationScreen() {
+    const [fontsLoaded] = useFonts({
+        'Roboto-bold': require('../assets/fonts/roboto/Roboto-Bold.ttf'),
+        'Roboto-regular': require('../assets/fonts/roboto/Roboto-Regular.ttf'),
+        'Roboto-medium': require('../assets/fonts/roboto/Roboto-Medium.ttf'),
+    });
+
+    const { pusher } = styles;
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <>
+            <View style={pusher} />
+            <Container>
+                <PhotoBox>
+                    <AddAvatarBtn onPress={() => Alert.alert('Add avatar')}>
+                        <Image source={AddIcon} />
+                    </AddAvatarBtn>
+                </PhotoBox>
+                <FormWrapper>
+                    <TextCenter>Реєстрація</TextCenter>
+                    <Input placeholder="Логін" />
+                    <Input placeholder="Адреса електронної пошти" />
+                    <Input placeholder="Пароль" />
+                    <RegisterButton onPress={() => Alert.alert('Register')}>
+                        <RegisterButtonText>Зареєструватись</RegisterButtonText>
+                    </RegisterButton>
+                    <Pressable onPress={() => Alert.alert('LogIn')}>
+                        <LoginNavigateButtonText>
+                            Вже є обліковий запис? Увійти
+                        </LoginNavigateButtonText>
+                    </Pressable>
+                </FormWrapper>
+            </Container>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    pusher: {
+        flex: 1,
+    },
+});
